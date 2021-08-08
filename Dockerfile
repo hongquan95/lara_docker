0bd858906f8523aebd94b2f40943da0829b83e74
hongquan95 <hongquan95.tran@gmail.com>
2021-08-09 02:22:45 +0700

    add test

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM php:8.0.5-fpm-alpine
+
+
+RUN docker-php-ext-install pdo pdo_mysql
+
+RUN curl -sS https://getcomposer.org/installer​ | php -- --install-dir=/usr/local/bin --filename=composer
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+WORKDIR /app
+COPY . .
+COPY .env.example .env
+RUN composer install && php artisan key:generate
---
+++ tests/Unit/FirstTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(1==1);
+        $this->assertTrue(1+1==2);
+        $this->assertTrue(1==1);
+    }
+}
